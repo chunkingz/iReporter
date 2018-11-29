a18c4de7c88b2ea2082fcda9191efeacbae0e81d
chunkingz <fortuneking33@yahoo.com>
2018-11-29 14:12:17 +0100

    chore(api): Add dummy data
    - Add dummy static data using js array/objects
    - Make sure the structure corresponds with the provided structure in the document
    
    [Finishes: 162306117]

---
+++ server/data/users.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    id: 1,
+    firstname: 'Fortune',
+    lastname: 'King',
+    othernames: 'Joshua',
+    email: 'chun@mail.com',
+    phoneNumber: '2349039933771',
+    username: 'chun',
+    password: 'password',
+    registered: new Date().toDateString(),
+    isAdmin: true,
+  },
+  {
+    id: 2,
+    firstname: 'Kenneth',
+    lastname: 'King',
+    othernames: 'Ebenezer',
+    email: 'ken@mail.com',
+    phoneNumber: '2349039933772',
+    username: 'ken',
+    password: 'password',
+    registered: new Date().toDateString(),
+    isAdmin: false,
+  },
+  {
+    id: 3,
+    firstname: 'Gabriel',
+    lastname: 'King',
+    othernames: 'Emmanuel',
+    email: 'gab@mail.com',
+    phoneNumber: '2349039933773',
+    username: 'gab',
+    password: 'password',
+    registered: new Date().toDateString(),
+    isAdmin: false,
+  },
+];
+
+export default users;
