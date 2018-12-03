1a9edc418e3f1631668a7228eb658acd3bd444ba
chunkingz <fortuneking33@yahoo.com>
2018-12-03 13:43:29 +0100

    chore(readmeFile): refactor readme file
    - add the server port number from env file
    
    [Delivers #162380128]

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.all('*', (req, res) => {
 });
 
 // start server on port
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log('iReporter app running on port.', server.address().port);
 });
